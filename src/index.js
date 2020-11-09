ff7feb4c6c5ed3f093c0ab5458f434f7efd79d01
sagbeyeg <agbeyegbesisan@gmail.com>
2020-11-09 18:52:10 -0500

    Data is displayed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 fetch("http://localhost:3000/api/v1/users/24")
   .then(r => r.json())
-  .then(console.log)
+  .then(data => {
+    renderUser(data)
+    renderPosts(data)
+  })
 
 
 // fetch("http://localhost:3000/api/v1/users/24", {
@@ -9,8 +12,42 @@ fetch("http://localhost:3000/api/v1/users/24")
 //     "Content-Type": "application/json"
 //   },
 //   body: JSON.stringify({ 
-//     name: "updated name"
+//     name: "Cordelia Chases"
 //   })
 // })
 //   .then(r => r.json())
 //   .then(console.log)
+
+const renderUser = user => {
+  // const userInfo = document.querySelector("#user-info")
+
+  const name = document.querySelector("#user-name")
+  name.textContent = user.name
+
+  const age = document.querySelector("#user-age")
+  age.textContent = user.age
+
+  const bio = document.querySelector("#user-bio")
+  bio.textContent = user.bio
+
+  
+
+  // renderUser(user)
+}
+
+
+const createPostCard = (post, postCards) => {
+  const card = document.createElement("card")
+  card.innerHTML = `<h3>${post.title}</h3>  <p>${post.content}</p>`
+  
+  postCards.append(card)
+}
+
+const renderPosts = user => {
+  const postCards = document.querySelector("main")
+  postCards.innerHTML = ""
+
+  user.posts.forEach(post => {
+    createPostCard(post, postCards)
+  })
+}
