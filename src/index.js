5bec3b183a5264b79fdd7037c103054b11c7b311
artwilton <Arthur.M.Wilton@gmail.com>
2020-11-10 14:46:01 -0500

    refactor frontend

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-fetch("http://localhost:3000/api/v1/users/1")
-  .then(r => r.json())
-  .then(data => {
-    renderUser(data)
-    renderPosts(data)
+const currentUserId = 1
+
+window.addEventListener('DOMContentLoaded', (event) => {
+  fetch(`http://localhost:3000/api/v1/users/${currentUserId}`)
+    .then(r => r.json())
+    .then(data => {
+      renderUser(data)
+      renderPosts(data)
+    })
+});
+
+// Fetch
+const updateUser = user => {
+  fetch(`http://localhost:3000/api/v1/users/${currentUserId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ 
+      name: "Cordelia Chases"
+    })
   })
+    .then(r => r.json())
+    .then(console.log)
+}
 
-
-fetch("http://localhost:3000/api/v1/users/1", {
-  method: "PATCH",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({ 
-    name: "Cordelia Chases"
-  })
-})
-  .then(r => r.json())
-  .then(console.log)
-
+// User Helper Methods
 const renderUser = user => {
   // const userInfo = document.querySelector("#user-info")
 
@@ -35,7 +42,7 @@ const renderUser = user => {
   // renderUser(user)
 }
 
-
+// Post Helper Methods
 const createPostCard = (post, postCards) => {
   const card = document.createElement("card")
   card.innerHTML = `<h3>${post.title}</h3>  <p>${post.content}</p>`
@@ -45,7 +52,6 @@ const createPostCard = (post, postCards) => {
 
 const renderPosts = user => {
   const postCards = document.querySelector("main")
-  // postCards.innerHTML = ""
 
   user.posts.forEach(post => {
     createPostCard(post, postCards)
